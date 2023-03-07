26ef05f88eced7f97a0e5953662f9d204abb21af
Marharyta Ponomarenko <margo.marharet@gmail.com>
2023-03-07 14:10:56 +0200

    Revert fete methodature: added non-interactive mode"
    This reverts commit 10721c4427f7c715eaf97632284f777ff2ff62f2.
    
    edited calculate method

--- Lab1.cpp
+++ Lab1.cpp
@@ -9,16 +9,13 @@
 using namespace std;
 
 void interactiveMode();
-void noninteractiveMode(char *argv);
-
 vector<double> validChecker();
 
 int main(int argc, char *argv[])
 {
     if (argc > 1)
     {
-        char *filename = argv[1];
-        noninteractiveMode(filename);
+        // implement non-interactive mode
     }
     else
     {
@@ -82,7 +79,7 @@ vector<double> validChecker()
             cin.clear();
             string errorText;
             cin >> errorText;
-            cout << "Error. Expected a valid real number, got " << errorText << " instead" << endl;
+            cout << "Error. Expected a valid real number, got " << errorText << " instead" << std::endl;
         }
     } while (!validInput);
     variables.push_back(a);
@@ -90,50 +87,3 @@ vector<double> validChecker()
     variables.push_back(c);
     return variables;
 };
-
-void noninteractiveMode(char *argv)
-{
-    fstream newfile;
-    newfile.open(argv, ios::in);
-    string readData;
-    if (newfile.is_open())
-    {
-        getline(newfile, readData);
-        newfile.close();
-        cout << readData << endl;
-        istringstream iss(readData);
-        double a, b, c, discriminant, x1, x2;
-        ;
-        iss >> a >> b >> c;
-        if (!iss.fail())
-        {
-            stringstream outputEquation;
-            outputEquation << fixed << setprecision(1) << "Equation is: (" << a << ") x^2 + (" << b << ") x + (" << c << ") = 0\n";
-            cout << outputEquation.str();
-            discriminant = pow(b, 2) - 4 * a * c;
-            if (discriminant > 0)
-            {
-                cout << "There are 2 roots\n";
-                x1 = (-b + sqrt(discriminant)) / (2 * a);
-                cout << fixed << setprecision(1) << "x1 = " << x1 << endl;
-                x2 = (-b - sqrt(discriminant)) / (2 * a);
-                cout << fixed << setprecision(1) << "x2 = " << x2 << endl;
-            }
-            if (discriminant == 0)
-            {
-                cout << "There are 1 roots\n";
-                x1 = -b / (2 * a);
-                cout << fixed << setprecision(1) << "x1 = " << x1 << endl;
-            }
-            if (discriminant < 0)
-            {
-                cout << "There are 0 roots\n";
-            }
-        }
-        cout << "invalid file format" << endl;
-    }
-    else
-    {
-        cout << "file does not exist" << endl;
-    }
-}
